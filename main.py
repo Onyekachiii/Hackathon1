5237140db08f3796b42b609695c1c0606037dc30
Sythe756 <138199234+Sythe756@users.noreply.github.com>
2023-08-05 13:06:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,157 +3,134 @@ import numpy as np
 import sys
 
 # Delay printing
-
 def delay_print(s):
-    # print one character at a time
-    # https://stackoverflow.com/questions/9246076/how-to-print-one-character-at-a-time-on-one-line
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
         time.sleep(0.05)
 
+Player1 = input("Player1 enter your name: ") or "This human hopefully its a human O>O, is way wayyy wayyyyyyyyyyyyyy too lazy to put a name and think they are funny so this will be your name now and no most certainly not you cannot change your name ;>"
+Player2 = input("Player2 enter your name: ") or "This human hopefully its a human O>O, is way wayyy wayyyyyyyyyyyyyy too lazy to put a name and think they are funny so this will be your name now and no most certainly not you cannot change your name ;>"
+
 # Create the class
 class Pokemon:
-    def __init__(self, name, types, moves, EVs, health='==================='):
-        # save variables as attributes
+    def __init__(self, name, types, moves, EVs, health='================================='):
         self.name = name
         self.types = types
         self.moves = moves
         self.attack = EVs['ATTACK']
         self.defense = EVs['DEFENSE']
         self.health = health
-        self.bars = 20 # Amount of health bars
-
+        self.bars = 20  # Amount of health bars
 
     def fight(self, Pokemon2):
-        # Allow two pokemon to fight each other
-
-        # Print fight information
-        print("-----POKEMONE BATTLE-----")
-        print(f"\n{self.name}")
-        print("TYPE/", self.types)
-        print("ATTACK/", self.attack)
-        print("DEFENSE/", self.defense)
-        print("LVL/", 3*(1+np.mean([self.attack,self.defense])))
-        print("\nVS")
-        print(f"\n{Pokemon2.name}")
-        print("TYPE/", Pokemon2.types)
-        print("ATTACK/", Pokemon2.attack)
-        print("DEFENSE/", Pokemon2.defense)
-        print("LVL/", 3*(1+np.mean([Pokemon2.attack,Pokemon2.defense])))
+        delay_print(f"{Player1} challenges {Player2}\n")
+        delay_print("\n-----POKEMON BATTLE-----")
+        delay_print(f"\n{Player1} chooses {self.name}")
+        delay_print(f"\nTYPE/{self.types}")
+        delay_print(f"\nATTACK/{self.attack}")
+        delay_print(f"\nDEFENSE/{self.defense}")
+        delay_print(f"\nLVL/{3 * (1 + np.mean([self.attack, self.defense]))}\n")
+        delay_print("\nVS\n")
+        delay_print(f"\n{Player2} chooses {Pokemon2.name}")
+        delay_print(f"\nTYPE/{Pokemon2.types}")
+        delay_print(f"\nATTACK/{Pokemon2.attack}")
+        delay_print(f"\nDEFENSE/{Pokemon2.defense}")
+        delay_print(f"\nLVL/{3 * (1 + np.mean([Pokemon2.attack, Pokemon2.defense]))}\n")
 
         time.sleep(2)
 
-        # Consider type advantages
         version = ['Fire', 'Water', 'Grass']
-        for i,k in enumerate(version):
+        for i, k in enumerate(version):
             if self.types == k:
-                # Both are same type
                 if Pokemon2.types == k:
                     string_1_attack = '\nIts not very effective...'
                     string_2_attack = '\nIts not very effective...'
-
-                # Pokemon2 is STRONG
-                if Pokemon2.types == version[(i+1)%3]:
+                if Pokemon2.types == version[(i + 1) % 3]:
                     Pokemon2.attack *= 2
                     Pokemon2.defense *= 2
                     self.attack /= 2
                     self.defense /= 2
                     string_1_attack = '\nIts not very effective...'
                     string_2_attack = '\nIts super effective!'
-
-                # Pokemon2 is WEAK
-                if Pokemon2.types == version[(i+2)%3]:
+                if Pokemon2.types == version[(i + 2) % 3]:
                     self.attack *= 2
                     self.defense *= 2
                     Pokemon2.attack /= 2
                     Pokemon2.defense /= 2
                     string_1_attack = '\nIts super effective!'
                     string_2_attack = '\nIts not very effective...'
+        delay_print(f"\n{Player1}:Go {self.name}!\n")
+        delay_print(f"\n{Player2}:Go {Pokemon2.name}!\n")
+        while self.bars > 0 and Pokemon2.bars > 0:
+            money = np.random.choice(1000)
+            delay_print(f"\n{self.name}: \n")
+            delay_print(f"HLTH:     {self.health}\n")
+            delay_print(f"\n{Pokemon2.name}: \n")
+            delay_print(f"HLTH:     {Pokemon2.health}\n")
 
-
-        # Now for the actual fighting...
-        # Continue while pokemon still have health
-        while (self.bars > 0) and (Pokemon2.bars > 0):
-            # Print the health of each pokemon
-            print(f"\n{self.name}\t\tHLTH\t{self.health}")
-            print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
-
-            print(f"Go {self.name}!")
+            delay_print(f"\n{Player1}:{self.name}, Use: ")
             for i, x in enumerate(self.moves):
-                print(f"{i+1}.", x)
-            index = int(input('Pick a move: '))
-            delay_print(f"\n{self.name} used {self.moves[index-1]}!")
-            time.sleep(1)
-            delay_print(string_1_attack)
+                delay_print(f"\n{i + 1}. {x}")
+            try:
+                index = int(input('\nPick a move: '))
+                delay_print(f"\n{self.name} used {self.moves[index - 1]}!")
+                time.sleep(1)
+                delay_print(f"\n{string_1_attack}")
+            except (IndexError, ValueError):
+                delay_print("Invalid input. Please pick a valid move.Since you are someone who doesnt understand basic english we skipped your turn and  selected a random move. :)")
 
-            # Determine damage
             Pokemon2.bars -= self.attack
-            Pokemon2.health = ""
+            Pokemon2.health = "=" * int(Pokemon2.bars + 0.1 * Pokemon2.defense)
 
-            # Add back bars plus defense boost
-            for j in range(int(Pokemon2.bars+.1*Pokemon2.defense)):
+            for j in range(int(Pokemon2.bars + 0.1 * Pokemon2.defense)):
                 Pokemon2.health += "="
 
             time.sleep(1)
-            print(f"\n{self.name}\t\tHLTH\t{self.health}")
-            print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
-            time.sleep(.5)
+            delay_print(f"\n{self.name}: \n")
+            delay_print(f"HLTH:     {self.health}\n")
+            delay_print(f"\n{Pokemon2.name}: \n")
+            delay_print(f"HLTH:     {Pokemon2.health}\n")
+            time.sleep(0.5)
 
-            # Check to see if Pokemon fainted
             if Pokemon2.bars <= 0:
-                delay_print("\n..." + Pokemon2.name + ' fainted.')
-                break
-
-            # Pokemon2s turn
-
-            print(f"Go {Pokemon2.name}!")
+                delay_print(f"\n{Pokemon2.name} fainted. {Player1} win!\n")
+                delay_print(f"\n{Player2} paid {Player1} ${money}.\n")
+                return
+            delay_print(f"\n{Player2}: {Pokemon2.name}, Use: ")
             for i, x in enumerate(Pokemon2.moves):
-                print(f"{i+1}.", x)
-            index = int(input('Pick a move: '))
-            delay_print(f"\n{Pokemon2.name} used {Pokemon2.moves[index-1]}!")
-            time.sleep(1)
-            delay_print(string_2_attack)
+                delay_print(f"\n{i + 1}. {x}")
+            try:
+                index = int(input('\nPick a move: '))
+                delay_print(f"\n{Pokemon2.name} used {Pokemon2.moves[index - 1]}!")
+                time.sleep(1)
+                delay_print(f"\n{string_2_attack}")
+            except (IndexError, ValueError):
+                delay_print("Invalid input. Please pick a valid move.Since you are someone who doesnt understand basic english we skipped your turn and  selected a random move. :)")
 
-            # Determine damage
             self.bars -= Pokemon2.attack
-            self.health = ""
+            self.health = "=" * int(self.bars + 0.1 * self.defense)
 
-            # Add back bars plus defense boost
-            for j in range(int(self.bars+.1*self.defense)):
+            for j in range(int(self.bars + 0.1 * self.defense)):
                 self.health += "="
 
-            time.sleep(1)
-            print(f"{self.name}\t\tHLTH\t{self.health}")
-            print(f"{Pokemon2.name}\t\tHLTH\t{Pokemon2.health}\n")
-            time.sleep(.5)
-
-            # Check to see if Pokemon fainted
             if self.bars <= 0:
-                delay_print("\n..." + self.name + ' fainted.')
-                break
-
-        money = np.random.choice(5000)
-        delay_print(f"\nOpponent paid you ${money}.\n")
-
-
-
-
-
+                delay_print(f"\n{self.name} fainted. {Player2} wins!\n")
+                delay_print(f"\n{Player1} paid {Player2} ${money}.\n")
+                return
 
 if __name__ == '__main__':
-    #Create Pokemon
-    Charizard = Pokemon('Charizard', 'Fire', ['Flamethrower', 'Fly', 'Blast Burn', 'Fire Punch'], {'ATTACK':12, 'DEFENSE': 8})
-    Blastoise = Pokemon('Blastoise', 'Water', ['Water Gun', 'Bubblebeam', 'Hydro Pump', 'Surf'],{'ATTACK': 10, 'DEFENSE':10})
-    Venusaur = Pokemon('Venusaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Earthquake', 'Frenzy Plant'],{'ATTACK':8, 'DEFENSE':12})
+    # Create Pokemon
+    Charizard = Pokemon('Charizard', 'Fire', ['Flamethrower', 'Fly', 'Blast Burn', 'Fire Punch'], {'ATTACK': 12, 'DEFENSE': 8})
+    Blastoise = Pokemon('Blastoise', 'Water', ['Water Gun', 'Bubblebeam', 'Hydro Pump', 'Surf'], {'ATTACK': 10, 'DEFENSE': 10})
+    Venusaur = Pokemon('Venusaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Earthquake', 'Frenzy Plant'], {'ATTACK': 8, 'DEFENSE': 12})
 
-    Charmander = Pokemon('Charmander', 'Fire', ['Ember', 'Scratch', 'Tackle', 'Fire Punch'],{'ATTACK':4, 'DEFENSE':2})
-    Squirtle = Pokemon('Squirtle', 'Water', ['Bubblebeam', 'Tackle', 'Headbutt', 'Surf'],{'ATTACK': 3, 'DEFENSE':3})
-    Bulbasaur = Pokemon('Bulbasaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Tackle', 'Leech Seed'],{'ATTACK':2, 'DEFENSE':4})
+    Charmander = Pokemon('Charmander', 'Fire', ['Ember', 'Scratch', 'Tackle', 'Fire Punch'], {'ATTACK': 4, 'DEFENSE': 2})
+    Squirtle = Pokemon('Squirtle', 'Water', ['Bubblebeam', 'Tackle', 'Headbutt', 'Surf'], {'ATTACK': 3, 'DEFENSE': 3})
+    Bulbasaur = Pokemon('Bulbasaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Tackle', 'Leech Seed'], {'ATTACK': 2, 'DEFENSE': 4})
 
-    Charmeleon = Pokemon('Charmeleon', 'Fire', ['Ember', 'Scratch', 'Flamethrower', 'Fire Punch'],{'ATTACK':6, 'DEFENSE':5})
-    Wartortle = Pokemon('Wartortle', 'Water', ['Bubblebeam', 'Water Gun', 'Headbutt', 'Surf'],{'ATTACK': 5, 'DEFENSE':5})
-    Ivysaur = Pokemon('Ivysaur\t', 'Grass', ['Vine Wip', 'Razor Leaf', 'Bullet Seed', 'Leech Seed'],{'ATTACK':4, 'DEFENSE':6})
+    Charmeleon = Pokemon('Charmeleon', 'Fire', ['Ember', 'Scratch', 'Flamethrower', 'Fire Punch'], {'ATTACK': 6, 'DEFENSE': 5})
+    Wartortle = Pokemon('Wartortle', 'Water', ['Bubblebeam', 'Water Gun', 'Headbutt', 'Surf'], {'ATTACK': 5, 'DEFENSE': 5})
+    Ivysaur = Pokemon('Ivysaur', 'Grass', ['Vine Wip', 'Razor Leaf', 'Bullet Seed', 'Leech Seed'], {'ATTACK': 4, 'DEFENSE': 6})
 
-
-    Charizard.fight(Blastoise) # Get them to fight
+    Ivysaur.fight(Squirtle) # Get them to fight
