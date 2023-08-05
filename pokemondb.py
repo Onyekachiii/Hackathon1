ae3a529a5c2438a094dd1412a1b6c0a6e3c027ba
Onyekachiii <stiloonwa4u@yahoo.com>
2023-08-05 11:43:23 +0400

    Pokemon DB

---
+++ pokemondb.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+HOSTNAME = 'localhost'
+USERNAME = 'postgres'
+PASSWORD = 'Splendour01%'
+DATABASE = 'Pokemon'
+
+connection = psycopg2.connect(host=HOSTNAME, user=USERNAME, password=PASSWORD, dbname=DATABASE)
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM players LIMIT 20;")
+
+results = cursor.fetchall()
+
+print(results)
+
+connection.close()
